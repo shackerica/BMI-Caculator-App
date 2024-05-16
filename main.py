f5a97e2e27eff4d01847112b9bd46cf402e62652
Erica Shackelford <ericalshackelford@gmail.com>
2024-05-16 19:28:50 +0000

    Add prompts for user height and weight
    This commit adds prompts for users to input their height in meters and weight in kilograms.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # Body Mass Index (BMI) from a user's weight and height.
+
+# Ask for 1st input: enter the height in meters
+height = input("Enter your height in meters: ")
+
+# Ask for 2nd input: enter the weight in kg
+weight = input("Enter your weight in kg: ")
