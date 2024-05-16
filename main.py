e8292a45a60842392af0144e379425c03c4adad9
Erica Shackelford <ericalshackelford@gmail.com>
2024-05-16 21:54:29 +0000

    Add BMI calculation and input conversion logic
    Add BMI calculation logic and input conversion to complete the program

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ height = input("Enter your height in meters: ")
 
 # Ask for 2nd input: enter the weight in kg
 weight = input("Enter your weight in kg: ")
+
+# Convert the height to a float
+height_as_float = float(height)
+
+# Convert the weight to a integer
+weight_as_int = int(weight)
+
+# Caclulate the BMI
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+# Convert the BMI to a integer
+bmi_as_int = int(bmi)
+
+# Print the integer BMI
+print(bmi_as_int)
